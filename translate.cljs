1782ad4dd1d3f3eefb49f3a1fdeb1eb690147ed6
jonathan dannel <usixjad@gmail.com>
2021-03-07 20:39:04 -0500

    add a progress message while fetching

--- translate.cljs
+++ translate.cljs
@@ -9,33 +9,29 @@
 (defn trim [s] (subs s 4 (- (count s) 3)))
 
 (defn get-text-from-block [block-id]
-  (reset! original-text 
-          (-> 
-            (rd/q '[:find ?string 
-                      :in $ ?uid 
-                      :where [?b :block/uid ?uid]
-                             [?b :block/string ?string]]
-        		     block-id)
-            str
-            trim
-            )))
+  (reset! original-text
+          (->
+           (rd/q '[:find ?string
+                   :in $ ?uid
+                   :where [?b :block/uid ?uid]
+                   [?b :block/string ?string]]
+                 block-id)
+           str
+           trim)))
 
 (defn fetch-translation [lang text]
-	(-> (.fetch js/window "https://roam-translate.herokuapp.com/translate" 
-                {:method "POST"
-                 :headers {:content-type "application/json"}
-                 :body (.stringify js/JSON
-                                   {:lang lang :text text})})
-  (.then #(.json %))
-  (.then #(reset! translation (.-result %)))
-))
+  (-> (.fetch js/window "https://roam-translate.herokuapp.com/translate"
+              {:method "POST"
+               :headers {:content-type "application/json"}
+               :body (.stringify js/JSON
+                                 {:lang lang :text text})})
+      (.then #(.json %))
+      (.then #(reset! translation (.-result %)))))
 
 (defn render-translation [text]
-   	[:span "Translation: " text])
+  [:span "Translation: " text])
 
 (defn main [_ lang [_ block-id]]
-   (get-text-from-block block-id)
-   (when @original-text (fetch-translation lang @original-text))
-   (render-translation @translation))
-
-
+  (get-text-from-block block-id)
+  (when @original-text (fetch-translation lang @original-text))
+  (if (not @translation) [:span "(translating)"] (render-translation @translation)))
