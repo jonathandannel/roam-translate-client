2d285aa6be7955b0b5e0eefc587e5344f7490b4b
jonathan dannel <usixjad@gmail.com>
2021-03-07 23:31:11 -0500

    formatting, cleanup

--- translate.cljs
+++ translate.cljs
@@ -6,21 +6,21 @@
 (def translation (r/atom nil))
 (def api-url "https://roam-translate.herokuapp.com/translate")
 
-(defn trim [s] (subs s 4 (- (count s) 3)))
+(defn trim-text [s] (subs s 4 (- (count s) 3)))
 
 (defn get-text-from-block [block-id]
   (reset! original-text
-          (->
-           (rd/q '[:find ?string
-                   :in $ ?uid
-                   :where [?b :block/uid ?uid]
-                   [?b :block/string ?string]]
-                 block-id)
+          (-> (rd/q 
+                '[:find ?string
+                  :in $ ?uid
+                  :where [?b :block/uid ?uid]
+                         [?b :block/string ?string]]
+               block-id)
            str
-           trim)))
+           trim-text)))
 
 (defn fetch-translation [lang text]
-  (-> (.fetch js/window "https://roam-translate.herokuapp.com/translate"
+  (-> (.fetch js/window api-url
               {:method "POST"
                :headers {:content-type "application/json"}
                :body (.stringify js/JSON
@@ -29,9 +29,11 @@
       (.then #(reset! translation (.-result %)))))
 
 (defn render-translation [text]
-  [:span "Translation: " text])
+  (if text
+    [:span "Translation: " text]
+    [:span "(translating)"]))
 
 (defn main [_ lang [_ block-id]]
   (get-text-from-block block-id)
   (when @original-text (fetch-translation lang @original-text))
-  (if (not @translation) [:span "(translating)"] (render-translation @translation)))
+  (render-translation @translation))
